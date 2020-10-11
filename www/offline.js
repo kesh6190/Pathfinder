f24de9818a45e483a75ad4cd25c5bd8ec3ae3a60
Surjitsing Chundunsing <skchundunsing@gmail.com>
2020-10-11 14:50:24 +0400

    Add files via upload

---
+++ www/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1602411414,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"img/obstacle-sheet0.png",
+		"img/sprite-sheet0.png",
+		"img/cellmarker-sheet0.png",
+		"img/node-sheet0.png",
+		"img/node-sheet1.png",
+		"img/tiledbackground.png",
+		"img/roughterrain-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"pathfind.js"
+	]
+}
